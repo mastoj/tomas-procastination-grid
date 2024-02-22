04ed32ce033ce93fd3d6e1526ccdd80df4896c0a
Tomas Jansson <tomas.jansson@gmail.com>
2024-02-22 23:53:19 +0100

    Use some random real names

--- src/components/employee-grid/data.ts
+++ src/components/employee-grid/data.ts
@@ -1,4 +1,4 @@
-import { Project, EmployeeRecord } from "./types";
+import { Project, EmployeeRecord, Role } from "./types";
 
 // Generate a list of 4 projects
 const projects: Project[] = [
@@ -8,15 +8,58 @@ const projects: Project[] = [
   { id: 4, name: "Project 4" },
 ];
 
-// Generate a list of 20 employees
-const employees: EmployeeRecord[] = Array.from({ length: 20 }, (_, i) => ({
-  id: i,
-  name: `Employee ${i + 1}`,
-  allocationPercentage: Math.floor(Math.random() * 100),
-  allocationHours: Math.floor(Math.random() * 100),
-  project: projects[Math.floor(Math.random() * projects.length)].name,
-  role: Math.random() > 0.5 ? "Developer" : "Designer",
-}));
+// Get a random first name from a list of 13 names
+const getFirstName = () => {
+  const firstNames = [
+    "Marius",
+    "Håkon",
+    "Andreas",
+    "Mads",
+    "Eirik",
+    "Thomas",
+    "Jonas",
+    "Lars",
+    "Ole",
+    "Kjetil",
+    "Pål",
+    "Per",
+    "Geir",
+  ];
+  return firstNames[Math.floor(Math.random() * firstNames.length)];
+};
+
+// Get a random last name from a list of 13 names
+const getLastName = () => {
+  const lastNames = [
+    "Rødde",
+    "Bjørn",
+    "Hansen",
+    "Johansen",
+    "Olsen",
+    "Larsen",
+    "Andersen",
+    "Nilsen",
+    "Pedersen",
+    "Kristiansen",
+    "Karlsen",
+    "Johnsen",
+    "Eriksen",
+  ];
+  return lastNames[Math.floor(Math.random() * lastNames.length)];
+};
+// Generate a list of 20 employees where the name field is using a random first and last name
+const employees: EmployeeRecord[] = Array.from({ length: 20 }, (_, i) => {
+  return {
+    id: i + 1,
+    name: `${getFirstName()} ${getLastName()}`,
+    allocationPercentage: Math.floor(Math.random() * 100),
+    allocationHours: Math.floor(Math.random() * 100),
+    project: projects[Math.floor(Math.random() * projects.length)].name,
+    role: ["Developer", "Designer", "Manager"][
+      Math.floor(Math.random() * 3)
+    ] as Role,
+  };
+});
 
 // Fake db call
 export const getProjects = async (): Promise<Project[]> => {
