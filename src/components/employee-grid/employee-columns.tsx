1f628620794a0fcb3543d8e284c4739c0f33414c
Tomas Jansson <tomas.jansson@gmail.com>
2024-02-22 23:12:28 +0100

    Add sorting

--- src/components/employee-grid/employee-columns.tsx
+++ src/components/employee-grid/employee-columns.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { Button } from "../ui/button";
 import EmployeeEditModal from "./employee-edit";
 import { useState } from "react";
@@ -63,15 +63,45 @@ export const getColumns = (
 ): ColumnDef<EmployeeRecord>[] => {
   return [
     {
-      header: () => <div className="text-left font-bold">Name</div>,
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
       accessorKey: "name",
     },
     {
-      header: () => <div className="text-left font-bold">Project</div>,
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Email
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
       accessorKey: "project",
     },
     {
-      header: () => <div className="text-left font-bold">Role</div>,
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Role
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
       accessorKey: "role",
     },
     {
--- src/components/employee-grid/employee-table.tsx
+++ src/components/employee-grid/employee-table.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -26,10 +28,16 @@ export function EmployeeTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
